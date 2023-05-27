f9c573b39917053e114c59e442b5589794e2ae8d
Dimas Bayu Pratama <dimasbayu6401@gmail.com>
2023-05-27 10:59:55 +0700

    custom fonts, added header

--- app/index.js
+++ app/index.js
@@ -1,10 +1,21 @@
 import { SafeAreaView, View, Text } from "react-native";
+import { Stack } from "expo-router";
 
 const Home = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ backgroundColor: "#FAFAFC", flex: 1 }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: "#FAFAFC" },
+          headerShadowVisible: false,
+          headerTitle: "HotelSaver",
+          headerTitleStyle: {
+            fontFamily: "DMBold",
+          },
+        }}
+      />
       <View>
-        <Text>This is home!</Text>
+        <Text style={{ fontFamily: "DMBold" }}>This is home!</Text>
       </View>
     </SafeAreaView>
   );
