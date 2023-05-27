fa1c9400c84583acae367d9f14c7050b2a6de93c
Dimas Bayu Pratama <dimasbayu6401@gmail.com>
2023-05-27 10:39:11 +0700

    project setup

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { SafeAreaView, View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>This is home!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
